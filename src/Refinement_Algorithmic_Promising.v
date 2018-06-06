06d7667e2a27cc9db7239287db44667d45b61391
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2018-06-06 23:34:29 +0900

    Prove an easy theorem

--- src/Refinement_Algorithmic_Promising.v
+++ src/Refinement_Algorithmic_Promising.v
@@ -21,6 +21,26 @@ Require Import Algorithmic.
 
 Set Implicit Arguments.
 
+
+Lemma rtc_astep_rtc_step
+      eustep am1 am2
+      (STEPS: rtc (AMachine.step eustep) am1 am2):
+  rtc (Machine.step eustep) am1 am2.
+Proof.
+  induction STEPS.
+  - refl.
+  - econs; eauto. inv H. econs; eauto.
+Qed.
+
+Theorem algorithmic_to_promising
+        p m
+        (EXEC: AMachine.exec p m):
+  Machine.exec p m.
+Proof.
+  inv EXEC. econs; eauto.
+  apply rtc_astep_rtc_step in STEP. ss.
+Qed.
+
 Theorem algorithmic_deadlock_free
         m
         (CONSISTENT: AMachine.consistent m):
