ebe49761fa1fb4212bf20be594757c01dc42e600
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2018-11-10 00:40:10 +0900

    Progress

--- src/lcertify/FindCertify.v
+++ src/lcertify/FindCertify.v
@@ -30,32 +30,26 @@ Inductive certified_promise (tid:Id.t) (eu1:ExecUnit.t (A:=unit)) (loc:Loc.t) (v
     (STEP2: rtc (certify_step tid) eu1 eu2)
     (STEP3: write_step tid loc val view_pre eu2 eu3)
     (STEP4: rtc (certify_step tid) eu3 eu4)
+    (PROMISES: Local.promises (ExecUnit.local eu4) = bot)
     (MEM: eu2.(ExecUnit.mem) = eu1.(ExecUnit.mem) ++ mem2')
     (MEM2': Forall (fun msg => msg.(Msg.loc) <> loc) mem2')
+    (COH_PRE: (eu2.(ExecUnit.local).(Local.coh) loc).(View.ts) <= length eu1.(ExecUnit.mem))
     (VIEW_PRE: view_pre.(View.ts) <= length eu1.(ExecUnit.mem))
 .
 
-Theorem certified_promise_complete
-        tid (eu1 eu2:ExecUnit.t (A:=unit)) loc val ts
-        (STATE: eu1.(ExecUnit.state) = eu2.(ExecUnit.state))
-        (LOCAL: Local.promise loc val ts tid
-                              eu1.(ExecUnit.local) eu1.(ExecUnit.mem)
-                              eu2.(ExecUnit.local) eu2.(ExecUnit.mem))
-        (CERTIFY: certify tid eu2):
-  certified_promise tid eu1 loc val.
-Proof.
-Admitted.
-
-Lemma promise_sim_eu
+Lemma promise_src_sim_eu
       tid (eu1:ExecUnit.t (A:=unit)) loc val
       (WF: ExecUnit.wf tid eu1):
   exists eu2,
+    <<STATE: eu2.(ExecUnit.state) = eu1.(ExecUnit.state)>> /\
     <<STEP: Local.promise loc val (S (length eu1.(ExecUnit.mem))) tid
                           eu1.(ExecUnit.local) eu1.(ExecUnit.mem)
                           eu2.(ExecUnit.local) eu2.(ExecUnit.mem)>> /\
+    <<MSG: nth_error eu2.(ExecUnit.mem) (length eu1.(ExecUnit.mem)) = Some (Msg.mk loc val tid)>> /\
     <<SIM: sim_eu tid (length eu1.(ExecUnit.mem)) (Promises.set (S (length eu1.(ExecUnit.mem))) bot) eu2 eu1>>.
 Proof.
-  destruct eu1 as [st1 lc1 mem1]. eexists (ExecUnit.mk st1 _ _). splits; ss.
+  destruct eu1 as [st1 lc1 mem1]. eexists (ExecUnit.mk _ _ _). splits; ss.
+  { rewrite nth_error_app2, Nat.sub_diag; ss. }
   econs; ss.
   - admit. (* sim_state refl *)
   - econs; ss.
@@ -77,9 +71,29 @@ Proof.
       exfalso. apply c. rewrite Time.add_0_r. refl.
 Admitted.
 
+Lemma sim_eu_write_step
+      tid ts loc val view_pre tsp src_promises eu1 eu2 eu2'
+      (SIM: sim_eu tid ts src_promises eu1 eu2)
+      (STEP: write_step tid loc val view_pre eu2 eu2')
+      (SRC_PROMISES_BELOW: forall tsp' msg
+                             (TSP': Promises.lookup (S tsp') (Promises.unset (S tsp) src_promises))
+                             (MEM: nth_error eu1.(ExecUnit.mem) tsp' = Some msg),
+          (eu2'.(ExecUnit.local).(Local.coh) msg.(Msg.loc)).(View.ts) <= ts)
+      (TSP: nth_error eu1.(ExecUnit.mem) tsp = Some (Msg.mk loc val tid))
+      (WF1: ExecUnit.wf tid eu1)
+      (WF2: ExecUnit.wf tid eu2)
+      (VCAP: eu2'.(ExecUnit.local).(Local.vcap).(View.ts) <= ts)
+      (COH_PRE: (eu2.(ExecUnit.local).(Local.coh) loc).(View.ts) <= ts)
+      (VIEW_PRE: view_pre.(View.ts) <= ts):
+  exists eu1',
+    <<STEP: certify_step tid eu1 eu1'>> /\
+    <<SIM: sim_eu tid ts (Promises.unset (S tsp) src_promises) eu1' eu2'>>.
+Proof.
+Admitted.
+
 Theorem certified_promise_sound
         tid (eu1:ExecUnit.t (A:=unit)) loc val
-        (WF: ExecUnit.wf tid eu1)
+        (WF1: ExecUnit.wf tid eu1)
         (CERTIFY: certified_promise tid eu1 loc val):
   exists eu2 ts,
     <<STATE: eu1.(ExecUnit.state) = eu2.(ExecUnit.state)>> /\
@@ -89,12 +103,21 @@ Theorem certified_promise_sound
     <<CERTIFY: certify tid eu2>>.
 Proof.
   inv CERTIFY.
-  exploit promise_sim_eu; eauto. i. des. rename eu0 into eu1', STEP into STEP1', SIM into SIM1.
-  exploit sim_eu_rtc_step; eauto.
+  exploit rtc_certify_step_wf; try exact STEP2; eauto. intro WF2.
+  exploit write_step_wf; eauto. intro WF3.
+  exploit rtc_certify_step_wf; try exact STEP4; eauto. intro WF4.
+
+  (* promise the message. *)
+  exploit promise_src_sim_eu; try exact WF1; eauto. i. des. rename eu0 into eu1', STEP into STEP1', SIM into SIM1.
+  exploit (ExecUnit.promise_step_wf (A:=unit)); try exact WF1; eauto.
+  { econs; try exact STEP1'; ss. }
+  intro WF1'.
+
+  (* simulate the certified steps before writing the message. *)
+  exploit sim_eu_rtc_step; try exact SIM1; eauto.
   { i. revert TSP. rewrite Promises.set_o. condtac; ss. clear X. inv e.
-    admit. (* view_pre *)
+    rewrite MSG in MEM0. inv MEM0. s. i. apply COH_PRE.
   }
-  { admit. (* wf *) }
   { rewrite <- VIEW_PRE.
     destruct eu2 as [st2 lc2 mem2].
     destruct eu3 as [st3 lc3 mem3].
@@ -102,5 +125,76 @@ Proof.
     rewrite <- join_r, <- join_r, <- join_l. ss.
   }
   i. des. rename eu1'0 into eu2', STEP into STEP2', SIM into SIM2.
-  admit. (* [write] and [to-the-end] *)
+  exploit rtc_certify_step_wf; try exact STEP2'; eauto. intro WF2'.
+
+  (* simulate the write step. *)
+  exploit sim_eu_write_step; try exact SIM2; eauto.
+  { i. revert TSP'. rewrite Promises.unset_o, Promises.set_o. condtac; ss. rewrite X. clear X.
+    rewrite Promises.lookup_bot. ss.
+  }
+  { admit. (* mem monotone *) }
+  { admit. (* vcap *) }
+  replace (Promises.unset (S (length (ExecUnit.mem eu1))) (Promises.set (S (length (ExecUnit.mem eu1))) bot)) with (bot:Promises.t); cycle 1.
+  { apply Promises.ext. i. rewrite Promises.unset_o, Promises.set_o. condtac; ss. clear X. inv e.
+    apply Promises.lookup_bot.
+  }
+  i. des. rename eu1'0 into eu3', STEP into STEP3', SIM into SIM3.
+  exploit certify_step_wf; try exact STEP3'; eauto. intro WF3'.
+
+  (* simulate the certified steps after writing the message. *)
+  exploit sim_eu_rtc_step_bot; try exact SIM3; eauto.
+  { i. revert TSP. rewrite Promises.lookup_bot. ss. }
+  i. des. rename eu1'0 into eu4', STEP into STEP4'.
+
+  esplits; try exact STEP1'; ss. econs; [|exact PROMISES0].
+  etrans; eauto.
+Admitted.
+
+
+Lemma promise_tgt_sim_eu
+      tid (eu1 eu2:ExecUnit.t (A:=unit))
+      (WF: ExecUnit.wf tid eu1)
+      (STEP: ExecUnit.promise_step tid eu1 eu2):
+  sim_eu tid (length eu1.(ExecUnit.mem)) bot eu1 eu2.
+Proof.
+  destruct eu1 as [st1 lc1 mem1].
+  destruct eu2 as [st2 lc2 mem2].
+  inv STEP. inv LOCAL. inv MEM2. ss. subst. econs; ss.
+  econs; ss.
+  - admit. (* sim_state refl *)
+  - econs; ss.
+    + admit. (* sim_fwdbank *)
+    + admit. (* sim_exbank *)
+    + i. rewrite Promises.set_o. condtac; ss. clear X. inv e. lia.
+    + i. rewrite ? Promises.set_o, Promises.lookup_bot.
+      eapply Local_wf_promises_above; eauto. apply WF.
+  - econs; ss.
+    + rewrite app_nil_r. ss.
+    + i. revert TSP. rewrite Promises.lookup_bot. ss.
+    + i. destruct n1; ss.
+Admitted.
+
+Theorem certified_promise_complete
+        tid (eu1 eu2:ExecUnit.t (A:=unit)) loc val ts
+        (WF1: ExecUnit.wf tid eu1)
+        (STATE: eu1.(ExecUnit.state) = eu2.(ExecUnit.state))
+        (LOCAL: Local.promise loc val ts tid
+                              eu1.(ExecUnit.local) eu1.(ExecUnit.mem)
+                              eu2.(ExecUnit.local) eu2.(ExecUnit.mem))
+        (CERTIFY: certify tid eu2):
+  certified_promise tid eu1 loc val.
+Proof.
+  (* promise the message. *)
+  exploit promise_tgt_sim_eu; eauto.
+  { econs; eauto. }
+  intro SIM1.
+  exploit (ExecUnit.promise_step_wf (A:=unit)); eauto.
+  { econs; eauto. }
+  intro WF2.
+
+  (* TODO: simulate the certified steps before fulfilling the message. *)
+
+  (* TODO: simulate the fulfill step. *)
+
+  (* TODO: simulate the certified steps after fulfilling the message. *)
 Admitted.
